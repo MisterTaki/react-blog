93ca0ab0833fb812e13c2ea9f8d5401db8d4667c
高琦 <gaoqi@100tal.com>
2017-09-20 22:56:42 +0800

    :spades: add global style;

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import logo from './assets/logo.svg';
+import './style/global';
+
 
 const Title = styled.h1`
   font-size: 28px;
---
+++ src/style/global.js
@@ -0,0 +1,8 @@
+import { injectGlobal } from 'styled-components';
+
+// eslint-disable-next-line no-unused-expressions
+export default injectGlobal`
+  body {
+    margin: 0;
+  }
+`;
