d70a1d43cf7569ebfa641f59e6ff7a57ac7f6543
高琦 <gaoqi@100tal.com>
2017-10-02 15:09:21 +0800

    update;

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import { Header } from './components/common';
+import { Header, Main, Footer } from './components/Layout';
 import { Home, Archives, Tags, Projects, Comments, About } from './containers';
 
 import './style/global';
 
 const Blog = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 100%;
 `;
 
@@ -15,14 +17,15 @@ export default function () {
     <Router>
       <Blog>
         <Header />
-        <div>
+        <Main>
           <Route exact path="/" component={Home} />
           <Route path="/archives" component={Archives} />
           <Route path="/tags" component={Tags} />
           <Route path="/projects" component={Projects} />
           <Route path="/comments" component={Comments} />
           <Route path="/about" component={About} />
-        </div>
+        </Main>
+        <Footer />
       </Blog>
     </Router>
   );
