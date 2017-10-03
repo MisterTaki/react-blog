0f35e57c9bc42b18260e482eb21132bc7b9ee47f
高琦 <gaoqi@100tal.com>
2017-10-03 12:39:33 +0800

    update;

--- src/components/Blog/Header/Header.jsx
+++ src/components/Blog/Header/Header.jsx
@@ -1,80 +1,141 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { rem, clearFix } from 'polished';
+import geopattern from 'geopattern';
 import { common } from '@/style/colors';
 import { zIndex } from '@/style/variables';
+import { blog } from '@/router';
 import logo from '@/assets/logo.png';
-import NavLink from './NavLink';
 
-const HeaderContainer = styled.header`
-  position: relative;
-  height: ${rem('60px')};
-  flex: 0 0 auto;
-`;
+const Header = ({ path, isExact, pathname, className }) => {
+  const blogRouter = blog(path);
+  const activeRouter = blogRouter[pathname];
+  const title = activeRouter ? activeRouter.label : '';
+  const subTitle = activeRouter ? activeRouter.description : '';
 
-const Header = styled.div`
-  position: fixed;
-  width: 100%;
-  background-color: ${common.white};
-  box-shadow: 0 0 3px rgba(14, 14, 14, 0.26);
-  z-index: ${zIndex.header};
-`;
-
-const HeaderWrapper = styled.section`
-  max-width: 1200px;
-  height: ${rem('60px')};
-  margin: 0 auto;
-  padding: 0 ${rem('10px')};
-  ${clearFix()};
-`;
-
-const LogoLink = styled(Link)`
-  float: left;
-  height: ${rem('60px')};
-  display: flex;
-  align-items: center;
-`;
-
-const Logo = styled.div`
-  height: ${rem('46px')};
-  width: ${rem('46px')};
-  margin-right: 6px;
-  background: url(${logo}) no-repeat center;
-  background-size: contain;
-`;
-
-const LogoText = styled.span`
-  font-size: ${rem('24px')};
-  color: ${common.theme};
-`;
-
-const NavList = styled.nav`
-  float: right;
-  height: ${rem('60px')};
-  display: flex;
-  align-items: center;
-`;
-
-export default function () {
   return (
-    <HeaderContainer>
-      <Header>
-        <HeaderWrapper>
-          <LogoLink to="/blog">
-            <Logo />
-            <LogoText>Blog</LogoText>
-          </LogoLink>
-          <NavList>
-            <NavLink to="/blog" exact>Home</NavLink>
-            <NavLink to="/blog/archives">Archives</NavLink>
-            <NavLink to="/blog/tags">Tags</NavLink>
-            <NavLink to="/blog/projects">Projects</NavLink>
-            <NavLink to="/blog/comments">Comments</NavLink>
-            <NavLink to="/blog/About">About</NavLink>
-          </NavList>
-        </HeaderWrapper>
-      </Header>
-    </HeaderContainer>
+    <header className={className}>
+      <div className="fixed-container">
+        <section className="main-wrapper">
+          <Link className="logo-link" to="path">
+            <div className="logo" />
+            <span className="logo-text">Blog</span>
+          </Link>
+          <nav className="nav-list">
+            {
+              Object.keys(blogRouter).map(item => (
+                <NavLink
+                  className="nav-link"
+                  key={item}
+                  to={item}
+                  exact={blogRouter[item].isExact}
+                >{ blogRouter[item].label }</NavLink>
+              ))
+            }
+          </nav>
+        </section>
+      </div>
+      { !isExact &&
+        <div className="title-wrapper">
+          <h2 className="title">{ title }</h2>
+          <h5 className="sub-title">{ subTitle }</h5>
+        </div>
+      }
+    </header>
   );
-}
+};
+
+Header.propTypes = {
+  path: PropTypes.string.isRequired,
+  isExact: PropTypes.bool.isRequired,
+  pathname: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+export default styled(Header)`
+  position: relative;
+  height: ${({ isExact }) => (isExact ? rem('60px') : '')};
+  padding: ${({ isExact }) => (isExact ? '' : '80px 20px 25px 20px')};
+  flex: 0 0 auto;
+  background-image: ${({ isExact }) => (isExact ? '' : geopattern.generate(window.location.href).toDataUrl())};
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  text-align: center;
+
+  .fixed-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: ${({ isHome }) => (isHome ? common.white : '')};
+    box-shadow: 0 0 3px rgba(14, 14, 14, 0.26);
+    z-index: ${zIndex.header};
+  }
+
+  .main-wrapper {
+    max-width: 1200px;
+    height: ${rem('60px')};
+    margin: 0 auto;
+    padding: 0 ${rem('10px')};
+    ${clearFix()};
+  }
+
+  .logo-link {
+    float: left;
+    height: ${rem('60px')};
+    display: flex;
+    align-items: center;
+  }
+
+  .logo {
+    height: ${rem('46px')};
+    width: ${rem('46px')};
+    margin-right: 6px;
+    background: url(${logo}) no-repeat center;
+    background-size: contain;
+  }
+
+  .logo-text {
+    font-size: ${rem('24px')};
+    color: ${({ isExact }) => (isExact ? common.theme : common.white)};
+  }
+
+  .nav-list {
+    float: right;
+    height: ${rem('60px')};
+    display: flex;
+    align-items: center;
+  }
+
+  .nav-link {
+    display: block;
+    height: ${'36px'};
+    line-height: ${'36px'};
+    margin: 0 ${rem('10px')};
+    font-size: ${rem('16px')};
+    color: ${({ isExact }) => (isExact ? common.darkBlack : common.white)};
+
+    &:hover {
+      border-bottom: 3px solid ${common.theme};
+    }
+
+    &.active {
+      border-bottom: 3px solid ${common.theme};
+    }
+  }
+
+  .title {
+    font-size: ${rem('36px')};
+    color: ${common.white};
+    text-shadow: 0 0 40px #34495e;
+  }
+
+  .sub-title {
+    margin: 10px 0 20px;
+    font-size: ${rem('16px')};
+    color: ${common.white};
+  }
+`;
