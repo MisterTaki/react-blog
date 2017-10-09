a76c0af8572433e6466e130dd776aefe594a1c0d
高琦 <gaoqi@100tal.com>
2017-10-09 11:20:37 +0800

    update;

--- src/components/Blog/Main/Main.jsx
+++ src/components/Blog/Main/Main.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const MainContainer = styled.main`
-  position: relative;
-  flex: 1 0 auto;
-`;
-
-const MainWrapper = styled.div`
-  max-width: 750px;
-  margin: 0 auto;
-`;
-
-const Main = ({ children }) => (
-  <MainContainer>
-    <MainWrapper>
-      {children}
-    </MainWrapper>
-  </MainContainer>
+const Main = ({ children, className }) => (
+  <main className={className}>
+    <div className="main-wrapper">
+      { children }
+    </div>
+  </main>
 );
 
 Main.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string.isRequired,
 };
 
-export default Main;
+export default styled(Main)`
+  position: relative;
+  flex: 1 0 auto;
+
+  .main-wrapper {
+    max-width: 750px;
+    margin: 0 auto;
+  }
+`;
