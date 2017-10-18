1da10b9fadaf6f83f1b819c2dc0c4449fdec40aa
高琦 <gaoqi@100tal.com>
2017-10-18 17:52:02 +0800

    [update] add redux-devtools-extension;

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { client } from '@/service';
 import { callAPI, loading } from './middlewares';
 import * as moduleReducers from './modules';
 
+// https://github.com/zalmoxisus/redux-devtools-extension
 export default createStore(
   combineReducers(moduleReducers),
-  applyMiddleware(callAPI(client), loading),
+  composeWithDevTools(applyMiddleware(callAPI(client), loading)),
 );
