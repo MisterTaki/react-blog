3d2acf06c6e562a9966fe79aff85d87614feee82
高琦 <gaoqi@100tal.com>
2017-10-18 17:24:52 +0800

    [update] add react-redux's Provider;

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import router from './router';
+import store from './store';
 
 import './style/global';
 
 export default function () {
   return (
-    <Router>
-      <Switch>
-        <Redirect exact from="/" to="/blog" />
-        {router.map(item => (
-          <Route
-            key={item.label}
-            path={item.path ? item.path : ''}
-            component={item.component}
-            exact={item.isExact}
-          />
-        ))}
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/blog" />
+          {router.map(item => (
+            <Route
+              key={item.label}
+              path={item.path ? item.path : ''}
+              component={item.component}
+              exact={item.isExact}
+            />
+          ))}
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
